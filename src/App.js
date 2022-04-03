dc6652077f02b3609918dfa290a88d689a95c796
uday-g-ithub15 <mudayhasan@gmail.com>
2022-04-04 01:03:26 +0600

    Complete product-thumb

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Header from './components/Header/Header';
-import Home from './components/Home/Home';
-import Reviews from './components/Reviews/Reviews';
-import Dashboard from './components/Dashboard/Dashboard';
-import Blogs from './components/Blogs/Blogs';
-import About from './components/About/About';
-import Error from './components/Error/Error';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Reviews from "./components/Reviews/Reviews";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Blogs from "./components/Blogs/Blogs";
+import About from "./components/About/About";
+import Error from "./components/Error/Error";
 
 function App() {
   return (
-   <>
-  <Header/>
-  <Routes>
-    <Route path='/' element = {<Home/>}></Route>
-    <Route path='/home' element = {<Home/>}></Route>
-    <Route path='/reviews' element = {<Reviews/>}></Route>
-    <Route path='/dashboard' element = {<Dashboard/>}></Route>
-    <Route path='/blogs' element = {<Blogs/>}></Route>
-    <Route path='/about' element = {<About/>}></Route>
-    <Route path='*' element = {<Error/>}></Route>
-  </Routes>
-   </>
+    <div className="body">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/reviews" element={<Reviews />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/blogs" element={<Blogs />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="*" element={<Error />}></Route>
+      </Routes>
+    </div>
   );
 }
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import laptop from "../../images/macbook.jpg";
+import "./Home.css";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Hello from home</h1>
-        </div>
-    );
+  return (
+    <div className="product-container">
+      <div className="product-details">
+        <h2 style={{ color: "whitesmoke", fontStyle: "italic" }}>
+          Laptop zone
+        </h2>
+        <h2>Buy your next Laptop</h2>
+        <h2>Get your best product</h2>
+        <button>Show demo</button>
+      </div>
+      <div className="img">
+        <img src={laptop} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
