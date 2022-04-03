0cdc5bb58ab38f3935bbe608b7ad3d97123261dd
uday-g-ithub15 <mudayhasan@gmail.com>
2022-04-04 02:37:29 +0600

    Complete dashboard and litte css

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,37 +1,61 @@
-import React from 'react';
+import React from "react";
+import "./Blogs.css";
 
 const Blogs = () => {
-    return (
-        <div>
-            <section>
-                <h2>What is context api ?</h2>
-                <p><b>
-                    Context is a process that can make the props drilling easier.In basic react application, data passes parent component to child components as props.But if we want to passes data to child of child component,in that case the process 'props drilling' is not so easy. For removing this problem, now we can use context.Context is just like a global variable.
-                    </b></p>
-            </section>
-            <section>
-                <h2>What is semantic tag ?</h2>
-                <p><b>
-                    Semantic html elements are those that explain their meaning as human readable.It exactly defines its content.Some of semantic elements are <code>&lt;header&gt;</code>, <code>&lt;nav&gt;</code>, <code>&lt;section&gt;</code>, <code>&lt;article&gt;</code>, <code>&lt;aside&gt;</code>, and <code>&lt;footer&gt;</code> act more or less like <code>&lt;div&gt;</code> elements.Semantic elements makes code easier to read.It has greater accessibility.
-                    </b></p>
-            </section>
-            <section>
-                <h2>What is the difference among inline, block and inline-block elements ?</h2>
-                <article><b>
-                    <p>1. Inline elements</p>
-                    <p>It doesn't allows to set width and height on the element.Some of inline elements are <code>&lt;a&gt; , &lt;span&gt;</code> etc.</p>
-                    </b></article>
-                <article><b>
-                    <p>2.inline-Block elements</p>
-                    <p>It allows to set width and height on the element.But there are not elements that initially set up as inline-block.We can an element inline-block using <code>display:inline-block;</code></p>
-                    </b></article>
-                <article><b>
-                    <p>1. Block elements</p>
-                    <p>It starts on a new line and takes up full width.Some of block elements are <code>&lt;div&gt; , &lt;p&gt;</code> etc.</p>
-                    </b></article>
-                </section>
-        </div>
-    );
+  return (
+    <div className="blogs-container">
+      <section>
+        <h2>What is context api ?</h2>
+        <p>
+          Context is a process that can make the props drilling easier.In basic
+          react application, data passes parent component to child components as
+          props.But if we want to passes data to child of child component,in
+          that case the process 'props drilling' is not so easy. For removing
+          this problem, now we can use context.Context is just like a global
+          variable.
+        </p>
+      </section>
+      <section>
+        <h2>What is semantic tag ?</h2>
+        <p>
+          Semantic html elements are those that explain their meaning as human
+          readable.It exactly defines its content.Some of semantic elements are{" "}
+          <code>&lt;header&gt;</code>, <code>&lt;nav&gt;</code>,{" "}
+          <code>&lt;section&gt;</code>, <code>&lt;article&gt;</code>,{" "}
+          <code>&lt;aside&gt;</code>, and <code>&lt;footer&gt;</code> act more
+          or less like <code>&lt;div&gt;</code> elements.Semantic elements makes
+          code easier to read.It has greater accessibility.
+        </p>
+      </section>
+      <section>
+        <h2>
+          What is the difference among inline, block and inline-block elements ?
+        </h2>
+        <article>
+          <p>1. Inline elements</p>
+          <p>
+            It doesn't allows to set width and height on the element.Some of
+            inline elements are <code>&lt;a&gt; , &lt;span&gt;</code> etc.
+          </p>
+        </article>
+        <article>
+          <p>2.inline-Block elements</p>
+          <p>
+            It allows to set width and height on the element.But there are not
+            elements that initially set up as inline-block.We can an element
+            inline-block using <code>display:inline-block;</code>
+          </p>
+        </article>
+        <article>
+          <p>1. Block elements</p>
+          <p>
+            It starts on a new line and takes up full width.Some of block
+            elements are <code>&lt;div&gt; , &lt;p&gt;</code> etc.
+          </p>
+        </article>
+      </section>
+    </div>
+  );
 };
 
 export default Blogs;
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,11 +1,105 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import "./Dashboard.css";
+import {
+  Area,
+  AreaChart,
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 const Dashboard = () => {
-    return (
-        <div>
-            <h1>Hello from Dashboard</h1>
-        </div>
-    );
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch(`data.json`)
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  return (
+    <div className="chart-container">
+      <div>
+        <h2>Month wise sell</h2>
+        <LineChart
+          width={400}
+          height={400}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <Line type="monotone" dataKey="sell" stroke="#ffffff"></Line>
+          <XAxis dataKey="month"></XAxis>
+          <YAxis />
+          <CartesianGrid />
+        </LineChart>
+      </div>
+      <div>
+        <h2>
+          Invesment <b>vs</b> Revenue
+        </h2>
+        <AreaChart
+          width={400}
+          height={400}
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid />
+          <Tooltip />
+          <XAxis dataKey="month"></XAxis>
+          <YAxis />
+          <Area
+            type="monotone"
+            dataKey="investment"
+            stroke="#8884d8"
+            fill="#8884d8"
+          ></Area>
+          <Area
+            type="monotone"
+            dataKey="revenue"
+            stroke="#82ca9d"
+            fill="#82ca9d"
+          ></Area>
+        </AreaChart>
+      </div>
+      <div>
+        <h2>
+          Invesment <b>vs</b> Revenue
+        </h2>
+        <BarChart
+          width={400}
+          height={400}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="month" />
+          <YAxis />
+          <Tooltip />
+          <Bar dataKey="revenue" fill="#8884d8" />
+          <Bar dataKey="investment" fill="#82ca9d" />
+        </BarChart>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
